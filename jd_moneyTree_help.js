bcec262a85cb41651b2009495b951fcecf19c39f
张朝杰 <zhaojie.zhang@drgou.com>
2021-09-09 10:06:09 +0800

    修复cash, sgmh, bookshop, health, montyTree_help 助力

--- jd_moneyTree_help.js
+++ jd_moneyTree_help.js
@@ -268,7 +268,8 @@ function shareCodesFormat() {
     } else {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将为本脚本作者【zero205】助力\n`)
       const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
-      $.newShareCodes = inviteCodes[tempIndex].split('@');
+      // $.newShareCodes = inviteCodes[tempIndex].split('@');
+      $.newShareCodes = inviteCodes[tempIndex] ? inviteCodes[tempIndex].split('@') : [];
     }
     console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
